fcf51ed958619eefe47ab56925a601c1989bfb60
1059701824 <1059701824@qq.com>
2018-10-25 21:52:25 +0800

    update test

--- src/main/java/net/sppan/base/controller/admin/AdminIndexController.java
+++ src/main/java/net/sppan/base/controller/admin/AdminIndexController.java
@@ -34,7 +34,24 @@ public class AdminIndexController extends BaseController{
 		friend.put("age", 18);
 		friends.add(friend);
 		map.put("friends", friends);
-		//return "admin/welcome";
+		return "admin/welcome";
+	}
+	@RequestMapping(value = {"/test"})
+	public  String test(Map<String,Object> map){
+		map.put("name", "testname");
+		map.put("sex", 1);    //sex:性别，1：男；0：女；
+
+		// 模拟数据
+		List<Map<String, Object>> friends = new ArrayList<Map<String, Object>>();
+		Map<String, Object> friend = new HashMap<String, Object>();
+		friend.put("name", "xbq");
+		friend.put("age", 22);
+		friends.add(friend);
+		friend = new HashMap<String, Object>();
+		friend.put("name", "July");
+		friend.put("age", 18);
+		friends.add(friend);
+		map.put("friends", friends);
 		return "test";
 	}
 }
--- target/classes/templates/test.ftl
+++ target/classes/templates/test.ftl
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>welcome</title>
+<title>test</title>
 </head>
 <body>
 <center>
